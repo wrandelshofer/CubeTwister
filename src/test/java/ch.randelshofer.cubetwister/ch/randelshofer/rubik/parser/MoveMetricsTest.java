6a4789ae64ce6f5af986021b07d1ba6b51c20ab9
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2018-12-26 14:58:53 +0100

    Renames test methods.

--- src/test/java/ch.randelshofer.cubetwister/ch/randelshofer/rubik/parser/MoveMetricsTest.java
+++ src/test/java/ch.randelshofer.cubetwister/ch/randelshofer/rubik/parser/MoveMetricsTest.java
@@ -14,36 +14,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MoveMetricsTest {
     @TestFactory
-    public List<DynamicTest> testMetrics() {
+    public List<DynamicTest> testAccept() {
         return Arrays.asList(
-                DynamicTest.dynamicTest("1", () -> doMetrics("R", 1, 1, 1, 1, 1)),
-                DynamicTest.dynamicTest("2", () -> doMetrics("R2", 1, 1, 1, 2, 1)),
-                DynamicTest.dynamicTest("3", () -> doMetrics("R'", 1, 1, 1, 1, 1)),
-                DynamicTest.dynamicTest("4", () -> doMetrics("R R", 1, 1, 1, 2, 2)),
-                DynamicTest.dynamicTest("5", () -> doMetrics("R R R", 1, 1, 1, 1, 3)),
-                DynamicTest.dynamicTest("6", () -> doMetrics("R R R2", 0, 0, 0, 0, 3)),
-                DynamicTest.dynamicTest("10", () -> doMetrics("CR", 0, 0, 0, 0, 1)),
-                DynamicTest.dynamicTest("11", () -> doMetrics("CR2", 0, 0, 0, 0, 1)),
-                DynamicTest.dynamicTest("12", () -> doMetrics("CR'", 0, 0, 0, 0, 1)),
-                DynamicTest.dynamicTest("13", () -> doMetrics("R CR R", 1, 1, 1, 2, 3)),
-                DynamicTest.dynamicTest("24", () -> doMetrics("R U", 2, 2, 2, 2, 2)),
-                DynamicTest.dynamicTest("25", () -> doMetrics("R R U", 2, 2, 2, 3, 3)),
-                DynamicTest.dynamicTest("25", () -> doMetrics("R U R", 3, 3, 3, 3, 3)),
-                DynamicTest.dynamicTest("26", () -> doMetrics("R R U2", 2, 2, 2, 4, 3)),
-                DynamicTest.dynamicTest("27", () -> doMetrics("R U2 R", 3, 3, 3, 4, 3)),
-                DynamicTest.dynamicTest("34", () -> doMetrics("CR R R", 1, 1, 1, 2, 3)),
-                DynamicTest.dynamicTest("33", () -> doMetrics("R CU R", 2, 2, 2, 2, 3)),
-                DynamicTest.dynamicTest("34", () -> doMetrics("CU R R", 1, 1, 1, 2, 3)),
-                DynamicTest.dynamicTest("41", () -> doMetrics("(R)1", 1, 1, 1, 1, 1)),
-                DynamicTest.dynamicTest("42", () -> doMetrics("(R)2", 1, 1, 1, 2, 2)),
-                DynamicTest.dynamicTest("43", () -> doMetrics("(R)3", 1, 1, 1, 1, 3)),
-                DynamicTest.dynamicTest("44", () -> doMetrics("(R)4", 0, 0, 0, 0, 4)),
-                DynamicTest.dynamicTest("51", () -> doMetrics("R MR L'", 0, 0, 0, 0, 3)),
-                DynamicTest.dynamicTest("101", () -> doMetrics("MR2 MF2 MU2", 3, 3, 6, 12, 3))
+                DynamicTest.dynamicTest("1", () -> doAccept("R", 1, 1, 1, 1, 1)),
+                DynamicTest.dynamicTest("2", () -> doAccept("R2", 1, 1, 1, 2, 1)),
+                DynamicTest.dynamicTest("3", () -> doAccept("R'", 1, 1, 1, 1, 1)),
+                DynamicTest.dynamicTest("4", () -> doAccept("R R", 1, 1, 1, 2, 2)),
+                DynamicTest.dynamicTest("5", () -> doAccept("R R R", 1, 1, 1, 1, 3)),
+                DynamicTest.dynamicTest("6", () -> doAccept("R R R2", 0, 0, 0, 0, 3)),
+                DynamicTest.dynamicTest("10", () -> doAccept("CR", 0, 0, 0, 0, 1)),
+                DynamicTest.dynamicTest("11", () -> doAccept("CR2", 0, 0, 0, 0, 1)),
+                DynamicTest.dynamicTest("12", () -> doAccept("CR'", 0, 0, 0, 0, 1)),
+                DynamicTest.dynamicTest("13", () -> doAccept("R CR R", 1, 1, 1, 2, 3)),
+                DynamicTest.dynamicTest("24", () -> doAccept("R U", 2, 2, 2, 2, 2)),
+                DynamicTest.dynamicTest("25", () -> doAccept("R R U", 2, 2, 2, 3, 3)),
+                DynamicTest.dynamicTest("25", () -> doAccept("R U R", 3, 3, 3, 3, 3)),
+                DynamicTest.dynamicTest("26", () -> doAccept("R R U2", 2, 2, 2, 4, 3)),
+                DynamicTest.dynamicTest("27", () -> doAccept("R U2 R", 3, 3, 3, 4, 3)),
+                DynamicTest.dynamicTest("34", () -> doAccept("CR R R", 1, 1, 1, 2, 3)),
+                DynamicTest.dynamicTest("33", () -> doAccept("R CU R", 2, 2, 2, 2, 3)),
+                DynamicTest.dynamicTest("34", () -> doAccept("CU R R", 1, 1, 1, 2, 3)),
+                DynamicTest.dynamicTest("41", () -> doAccept("(R)1", 1, 1, 1, 1, 1)),
+                DynamicTest.dynamicTest("42", () -> doAccept("(R)2", 1, 1, 1, 2, 2)),
+                DynamicTest.dynamicTest("43", () -> doAccept("(R)3", 1, 1, 1, 1, 3)),
+                DynamicTest.dynamicTest("44", () -> doAccept("(R)4", 0, 0, 0, 0, 4)),
+                DynamicTest.dynamicTest("51", () -> doAccept("R MR L'", 0, 0, 0, 0, 3)),
+                DynamicTest.dynamicTest("101", () -> doAccept("MR2 MF2 MU2", 3, 3, 6, 12, 3))
         );
     }
 
-    private void doMetrics(String script, int expectedBtm, int expectedLtm, int expectedFtm, int expectedQtm, int expectedCount) throws IOException {
+    private void doAccept(String script, int expectedBtm, int expectedLtm, int expectedFtm, int expectedQtm, int expectedCount) throws IOException {
         System.out.println("doMetrics script: "+script);
         System.out.println("  expected: "+expectedBtm+"btm, "+expectedLtm+"ltm, "+expectedFtm+"ftm, "+expectedQtm+"qtm, "+expectedCount+" moves");
         ScriptParser parser = new ScriptParser(new DefaultNotation());
